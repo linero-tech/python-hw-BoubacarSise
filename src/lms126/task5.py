6e989a10e8baea0a93deab16f81b9d18d86fceae
Boubacar Sise <122878380+BoubacarSise@users.noreply.github.com>
2023-02-07 21:52:10 +0100

    Solution task5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -1,5 +1,21 @@
-from to_do import TODO
-
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    return """ 
+    INPUT X
+    IF X = 1
+    OUTPUT "Monday"
+    ELSE IF X = 2
+    OUTPUT "Tuesday"
+    ELSE IF X = 3
+    OUTPUT "Wednesday"
+    ELSE IF X = 4
+    OUTPUT "Thursday"
+    ELSE IF X = 5
+    OUTPUT "Friday"
+    ELSE IF X = 6
+    OUTPUT "Saturday"
+    ELSE IF X = 7
+    OUTPUT "Sunday"
+    ELSE
+    OUTPUT "Invalid day number"
+    """
