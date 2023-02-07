eb7f2fb8210d3a6ca9a45c11be23a9256caec153
Boubacar Sise <122878380+BoubacarSise@users.noreply.github.com>
2023-02-07 22:20:46 +0100

    Solution task8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,11 +2,13 @@
 def task8():
     return """
     INPUT X
-    IF X multiple 3 = 0 AND X * 5 = 0
+    IF X MOD 3 = 0 AND X MOD 5 = 0
     OUTPUT "FooBar"
-    ELSE IF X * 3 = 0
+    ELSE 
+    IF X MOD 3 = 0
     OUTPUT "Foo"
-    ELSE IF X * 5 = 0
+    ELSE 
+    IF X MOD 5 = 0
     OUTPUT "Bar"
     ELSE
     OUTPUT "Qix"
