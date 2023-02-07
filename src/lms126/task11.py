1402cb2fbdfa4f06fa245145abaf699aff431ce0
Boubacar Sise <122878380+BoubacarSise@users.noreply.github.com>
2023-02-07 21:54:15 +0100

    Solution task11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -1,5 +1,13 @@
-from to_do import TODO
-
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT temperature
+    IF temperature ends with "C"
+    temperature = temperature without "C"
+    temperature = (temperature * 9 / 5) + 32
+    OUTPUT temperature + "F"
+    ELSE IF temperature ends with "F"
+    temperature = temperature with "F"
+    temperature = (temperature - 32) * 5 / 9
+    OUTPUT temperature + "C"
+    """
