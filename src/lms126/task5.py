218c29c0a422b50ac4eb342daa3bbb6dfbd3752f
Boubacar Sise <122878380+BoubacarSise@users.noreply.github.com>
2023-02-07 22:16:06 +0100

    Solution task5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -4,17 +4,23 @@ def task5():
     INPUT X
     IF X = 1
     OUTPUT "Monday"
-    ELSE IF X = 2
+    ELSE 
+    IF X = 2
     OUTPUT "Tuesday"
-    ELSE IF X = 3
+    ELSE 
+    IF X = 3
     OUTPUT "Wednesday"
-    ELSE IF X = 4
+    ELSE 
+    IF X = 4
     OUTPUT "Thursday"
-    ELSE IF X = 5
+    ELSE 
+    IF X = 5
     OUTPUT "Friday"
-    ELSE IF X = 6
+    ELSE 
+    IF X = 6
     OUTPUT "Saturday"
-    ELSE IF X = 7
+    ELSE 
+    IF X = 7
     OUTPUT "Sunday"
     ELSE
     OUTPUT "Invalid day number"
