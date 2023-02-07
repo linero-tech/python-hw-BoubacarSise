cee551d997a9fe383abb384cf115fa5908d6737a
Boubacar Sise <122878380+BoubacarSise@users.noreply.github.com>
2023-02-07 21:53:49 +0100

    Solution task10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -1,5 +1,13 @@
-from to_do import TODO
-
 
 def task10():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT email, password
+    IF email already exist in the database
+    OUTPUT "Email already exists. Please use another email."
+    ELSE
+    IF length of password < 6
+    OUTPUT "Password must be at least 6 characters long."
+    ELSE
+    add_account(email, password)
+    OUTPUT "Account creation successful. Welcome to our service."
+    """
