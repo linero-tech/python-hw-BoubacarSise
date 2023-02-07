4a5ff3a56856332ef532d41ae4d085a6392055b2
Boubacar Sise <122878380+BoubacarSise@users.noreply.github.com>
2023-02-07 21:53:32 +0100

    Solution task9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -1,5 +1,16 @@
-from to_do import TODO
-
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """"
+    INPUT account_number, password, withdrawal_amount
+    IF account_number AND password NOT FOUND in the database
+    OUTPUT "Invalid account number or password."
+    ELSE
+    account_balance = retrieve_balance(account_number)
+    IF withdrawal_amount > account_balance
+    OUTPUT "Transaction failed. Insufficient funds."
+    ELSE
+    account_balance = account_balance - withdrawal_amount
+    update_balance(account_number, account_balance)
+    dispense_cash(withdrawal_amount)
+    OUTPUT "Withdrawal successful. Your new balance is: " + account_balance
+    """
